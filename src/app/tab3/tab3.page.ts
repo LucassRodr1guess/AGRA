3d1e19743d31d8a5e766a05762104ac3f54b698f
LucassRodr1guess <84428281+LucassRodr1guess@users.noreply.github.com>
2023-09-29 08:29:04 -0300

    Add files via upload

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
+  agricultureNews: any;
 
-  constructor() {}
+  constructor(public http: HttpClient) {
+    this.loadAgricultureNews();
+  }
 
+  loadAgricultureNews() {
+    const jsonFile = '../../assets/agriculture_news.json';
+
+    this.http.get(jsonFile).subscribe((data: any) => {
+      this.agricultureNews = data;
+    });
+  }
 }
+
+
+  
